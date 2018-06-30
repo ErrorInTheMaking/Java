479bf2a78d02e37930e076acb3138ea93cf2f585
ErrorInTheMaking <37042291+ErrorInTheMaking@users.noreply.github.com>
2018-06-30 16:08:45 +0300

    Update insertion_sort.java

--- insertion_sort.java
+++ insertion_sort.java
@@ -1,13 +1,12 @@
-
 public class insertion_sort {
 	public void insertionsort(int arr[]) {
 		for(int i = 1; i < arr.length; ++i) {			//For loop from 1 to length
 			int j = i;
-			while(j > 0 && arr[j-1] > arr[j]) {			//Check if previous index is greater than current 
-				int temp = arr[j];						//Swap
+			while(j > 0 && arr[j-1] > arr[j]) {		//Check if previous index is greater than current 
+				int temp = arr[j];			//Swap
 				arr[j] = arr[j-1];
 				arr[j-1] = temp;
-				--j;									//Decrease j after swapping
+				--j;					//Decrease j after swapping
 			}
 		}
 	}
@@ -24,7 +23,7 @@ public class insertion_sort {
 			System.out.print(arr[i] + " ");
 		System.out.println();
 			
-		is.insertionsort(arr);							//Apply insertion sort
+		is.insertionsort(arr);					//Apply insertion sort
 		
 		System.out.print("After insertion sort: ");		//Print after sorting
 		for(int i = 0;  i < arr.length; ++i)
