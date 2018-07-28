cb801f1f4c43178b86782943434515b1622e19e1
ErrorInTheMaking <37042291+ErrorInTheMaking@users.noreply.github.com>
2018-07-28 23:29:57 +0300

    Update Ninja.java

--- src/Ninja.java
+++ src/Ninja.java
@@ -11,5 +11,34 @@ public class Ninja extends Warrior{
 	@Override
 	public void setWarriorSound() {this.warriorSound = "Ninja Chop!";}
 	public String getWarriorSound() {return warriorSound;}
+	public void secretMessage() {if(this.getDefensivePower() > 100)this.warriorSound = "Super Sayan!";}
+	public void increaseAttackingPower(int power) {
+		int currentPower = this.getAttackingPower();
+		if(currentPower < MAX_ATTACKING_POWER) {
+			this.setAttackingPower(currentPower + power + 40);
+			this.secretMessage();
+			System.out.println("A ninja doesn't lose defensive power!" );
+			System.out.println(this.getName() + ": Attacking power upgraded to " + this.getAttackingPower()  + "(+" + power + ")");
+			this.checkDefensivePower();
+		}
+		else {
+			System.out.println(this.getName() + ": Attacking power is maxed out.Resetting to 50");
+			this.setAttackingPower(50);
+		}
+		findMaxAttackingPower();
+	}
+	public void increaseDefensivePower(int power) {
+		int currentPower = this.getDefensivePower();
+		int rand = (int)(Math.random()*21);
+		if(currentPower < MAX_DEFENSIVE_POWER) {
+			this.setDefensivePower(currentPower + power + rand);
+			this.secretMessage();
+			System.out.println(this.getName() + ": Defensive power upgraded to:" + this.getDefensivePower() + "(+" + (power+rand) + ")");
+		}
+		else {
+			System.out.println(this.getName() + ": Defensive power is maxed out.Resetting to 50");
+			this.setDefensivePower(50);
+		}
+	}
 	
 }
